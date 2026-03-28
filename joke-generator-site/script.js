6ecbca1658633ebd67b76b1bc4eb07ada5a0c7de
siddharthajain5851 <siddharthajain5851@gmail.com>
2026-03-28 22:14:05 +0530

    Add files via upload

---
+++ joke-generator-site/script.js
@@ -0,0 +1,72 @@
+let a=()=>{
+    let n=(Math.random()*50)+3;
+    n=Math.floor(n);
+     
+    return n;
+
+}
+ let jokes = [
+  "Why don't scientists trust atoms? Because they make up everything!",
+  "Why don't eggs tell jokes? They'd crack each other up!",
+  "Why did the tomato turn red? Because it saw the salad dressing!",
+  "What do you call a fake noodle? An impasta!",
+  "Why did the scarecrow win an award? Because he was outstanding in his field!",
+  "Why don't lobsters share? Because they're shellfish!",
+  "What do you call a can opener that doesn't work? A can't opener!",
+  "I told my wife she was drawing her eyebrows too high. She looked surprised.",
+  "Why don't some couples go to the gym? Because some relationships don't work out!",
+  "What do you call a bear with no socks on? Barefoot!",
+  "Why did the bicycle fall over? Because it was two-tired!",
+  "I'm reading a book about anti-gravity. It's impossible to put down!",
+  "Why did the banana go to the doctor? Because he wasn't peeling well!",
+  "Why did the chicken cross the playground? To get to the other slide!",
+  "What do you call a group of cows playing instruments? A moo-sical band!",
+  "Why did the baker go to the bank? He needed dough!",
+  "Why did the mushroom go to the party? Because he was a fun-gi!",
+  "Why did the cat join a band? Because he wanted to be the purr-cussionist!",
+  "What do you call a dog that does magic tricks? A labracadabrador!",
+  "Why did the computer go to the doctor? It had a virus!",
+  "Why did the kid bring a ladder to school? He wanted to reach his full potential!",
+  "What do you call a fish with a sunburn? A star-fish!",
+  "Why did the turkey join the band? He was a drumstick!",
+  "Why did the orange stop in the middle of the road? It ran out of juice!",
+  "What do you call a cat that's a good listener? A purr-fect listener!",
+  "Why did the rabbit go to the doctor? He had hare-loss!",
+  "Why did the kid become a baker? He kneaded the dough!",
+  "What do you call a snowman with a six-pack? An abdominal snowman!",
+  "Why did the carrot go to the doctor? Because it was feeling a little root-less!",
+  "Why did the kid bring a magnet to school? He wanted to attract attention!",
+  "What do you call a cow with no legs? Ground beef!",
+  "Why did the chicken go to the gym? To get some egg-cellent abs!",
+  "Why did the baker make a cake in the shape of a chicken? It was an egg-cellent idea!",
+  "What do you call a penguin who's a good dancer? A penguin-shaker!",
+  "Why did the banana split? Because it wasn't peeling well under the pressure!",
+  "Why did the kid bring a pillow to school? So he could have a soft learning experience!",
+  "What do you call a cat that's a great listener and a good singer? A purr-fect harmony!",
+  "Why did the apple join the gym? To get some core strength!",
+  "Why did the kid become a chef? He loved egg-perimenting with new recipes!",
+  "What do you call a dog that's a great dancer? A paw-some dancer!",
+  "Why did the turkey go to the doctor? He had a fowl cough!",
+  "Why did the orange juice carton go to therapy? It was feeling a little crushed!",
+  "What do you call a cow that's a great singer? A moo-sician!",
+  "Why did the kid bring a compass to school? He wanted to navigate the curriculum!",
+  "Why did the baker go to the bank? He needed dough, and he couldn't loaf around!",
+ "What do you call a fish that's a great musician? A fin-tastic musician!",
+  "Why did the chicken go to the beauty parlor? It wanted a beak trim!",
+ " Why did the kid become a scientist? He was curious about the egg-istence of things!",
+  "What do you call a cat thats a great problem solver? A purr-fect solution!",
+  "Why did the banana go to the doctor? Because it was not peeling well, and it was going bananas!",
+  "Why did the scarecrow win a prize? Because he was outstanding in his field!",
+  "What do you call a group of eggs playing instruments? An egg-cellent band!"];
+   let kii=a();  
+     const k=document.querySelector("#j");
+         k.innerHTML= jokes[kii];
+    //   console.dir(k)
+      
+    //  let s= joke[kii]
+    //  alert(s);
+    // 
+    // if(jokes&&Array.isArray(jokes)){
+        // console.log(jokes[kii]);
+   // }
+      
